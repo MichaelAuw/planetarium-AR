3c81ee5867795882e9b557450f0216d752f689d4
MichaelAuw <michaelawarsa@gmail.com>
2023-03-30 00:14:40 +0700

    Update v2

---
+++ Assets/Start/Game/PlayManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    public GameObject Information;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Information.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReturnButtonClicked(){
+        Application.LoadLevel("Start");
+    }
+
+    public void InformationButtonClicked(){
+        if(Information.activeSelf == false){
+             Information.SetActive(true);
+        }
+        else{
+            Information.SetActive(false);
+        }
+    }
+}
